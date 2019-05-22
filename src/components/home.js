b5783dfda130ad6d83e9ef75d415822cdd8ec1b7
stharuma <stharuma@gmail.com>
2019-05-22 14:59:55 -0400

    display products list

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,13 @@
 import React,{Component} from 'react';
+import ProductListing from '../features/product-listing';
+
+import data from '../data/products.json'
 
 export default class Home extends Component {
     render(){
         return( <div>
-            <h1>It is home page !!!!</h1>
+            <h1>Home !!!!</h1>
+            <ProductListing  products={data.products}/>
         </div>);
     }
 }
